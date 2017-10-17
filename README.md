26f2d35a5c6bbc9276924e7508a2c40479319aef
Gabriel Redig & Guillermo Ramos-Macias <gredig@teachtci.com>
2017-10-17 15:16:56 -0700

    it's a start

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # oojs-pairing
 Let's do some OOJS
+
+Use the data array and OOJS to create a page that displays the information for each animal. 
+
+Include a button for each animal that will change the color of the animal name to the secondary color (and back when clicked again).
---
+++ app.js
@@ -0,0 +1,42 @@
+var animal_data = [
+	{
+		name: 'Dog',
+		legs: 4,
+		warm_blooded: true,
+		thumb: 'https://images-na.ssl-images-amazon.com/images/G/01/aplusautomation/vendorimages/a0eab96e-e586-4e59-a961-21ed7da6c5e6.jpg._CB282597861__SR285,285_.jpg',
+		color: '#ff0000',
+		secondary_color: '#880000'
+	},
+	{
+		name: 'Cat',
+		legs: 4,
+		warm_blooded: true,
+		thumb: 'http://www.petfood.co.uk/wp-content/uploads/2011/06/free-cat-food-samples.jpg',
+		color: '#00ff00',
+		secondary_color: '#008800'
+	},
+	{
+		name: 'Pig',
+		legs: 4,
+		warm_blooded: true,
+		thumb: 'http://www.offthegridnews.com/wp-content/uploads/2011/03/pigs-400x265.jpg',
+		color: '#0000ff',
+		secondary_color: '#000088'
+	},
+	{
+		name: 'Horse',
+		legs: 4,
+		warm_blooded: true,
+		thumb: 'https://thumbs.dreamstime.com/x/free-draft-horse-21238687.jpg',
+		color: '#f0000f',
+		secondary_color: '#800008'
+	},
+	{
+		name: 'snake',
+		legs: 0,
+		warm_blooded: false,
+		thumb: 'https://i2.wp.com/www.rachelpeachey.com/wp-content/uploads/2016/11/Snake-Book-and-Snake-Unit-Project-Ideas.png?resize=665%2C435',
+		color: '#0f00f0',
+		secondary_color: '#080080'
+	},
+]
